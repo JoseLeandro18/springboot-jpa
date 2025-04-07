4b8e999c7708181efb5d40a2d79508d3d6ade597
José Leandro <joseleand06@gmail.com>
2025-04-07 16:15:47 -0300

    Project Created

---
+++ src/main/java/com/estudos/springboot_jpa_web/SpringbootJpaWebApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot_jpa_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/springboot_jpa_web/SpringbootJpaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot_jpa_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
