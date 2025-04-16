438d38d8a1295e3848da7ae5d44ae413653ad5d6
José Leandro <joseleand06@gmail.com>
2025-04-16 10:40:29 -0300

    OrderStatus enum

--- src/main/java/com/estudos/springboot_jpa_web/config/TestConfig.java
+++ src/main/java/com/estudos/springboot_jpa_web/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.estudos.springboot_jpa_web.config;
 
 import com.estudos.springboot_jpa_web.entities.Order;
 import com.estudos.springboot_jpa_web.entities.User;
+import com.estudos.springboot_jpa_web.entities.enums.OrderStatus;
 import com.estudos.springboot_jpa_web.repositories.OrderRepository;
 import com.estudos.springboot_jpa_web.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User us1 = new User(null, "José Leandro", "jose@gmail.com", "11935041945", "123");
         User us2 = new User(null, "Isabele Fernandes", "isa@gmail.com", "35928453202", "321");
 
-        Order or1 = new Order(null, Instant.parse("2025-04-14T10:55:07Z"), us1);
-        Order or2 = new Order(null, Instant.parse("2025-04-14T13:55:13Z"), us2);
-        Order or3 = new Order(null, Instant.parse("2025-04-14T22:55:22Z"), us1);
+        Order or1 = new Order(null, Instant.parse("2025-04-14T10:55:07Z"), OrderStatus.PAID, us1);
+        Order or2 = new Order(null, Instant.parse("2025-04-14T13:55:13Z"), OrderStatus.WAITING_PAYMENT, us2);
+        Order or3 = new Order(null, Instant.parse("2025-04-14T22:55:22Z"), OrderStatus.WAITING_PAYMENT, us1);
 
         userRepository.saveAll(Arrays.asList(us1, us2));
         orderRepository.saveAll(Arrays.asList(or1, or2, or3));
